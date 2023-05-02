0d9acc3d828774104e3a6575d4151b4d0ba96485
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-05-02 17:22:21 -0300

    :sparkles: feat: feita validação com yup

--- src/Schema/schema.js
+++ src/Schema/schema.js
@@ -2,4 +2,28 @@ import * as yup from 'yup';
 
 export const schema = yup.object().shape({
   status: yup.string().required('Escolha uma opção!'),
+  tags: yup
+    .array()
+    .of(yup.string())
+    .when('status', {
+      is: 'reprovado',
+      then: (schema) =>
+        schema.when('comment', {
+          is: '',
+          then: (schema) =>
+            schema
+              .min(1, 'Selecione ao menos uma tag, ou deixe um comentário!')
+              .required('Selecione ao menos uma tag!'),
+        }),
+    }),
+  comment: yup.string().when('status', {
+    is: 'reprovado',
+    then: (schema) =>
+      schema.when('tags', {
+        is: [],
+        then: schema
+          .min(20, 'Deixe um comentário!')
+          .required('Deixe um comentário!'),
+      }),
+  }),
 });
